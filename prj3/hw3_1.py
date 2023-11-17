fa80f0a532ef1397b9c538855e34aafdf02fcd37
ddwkim <dwkim0824@gmail.com>
2023-11-18 02:03:22 +0900

    Update hw3 prob1

--- prj3/hw3_1.py
+++ prj3/hw3_1.py
@@ -1,28 +1,57 @@
 import sys
-import math
 
 from pyspark import SparkConf, SparkContext
 
 BETA = 0.9
+NUM_ITER = 50
+NUM_PRINT = 10
 
-conf = SparkConf()
-sc = SparkContext(conf=conf)
+
+def compute_contribs(dests, rank):
+    num_dests = len(dests)
+    for dest in dests:
+        yield (dest, rank / num_dests)
+
+
+def pagerank(rdd):
+    num_pages = rdd.count()
+    ranks = rdd.map(lambda x: (x[0], 1 / num_pages))
+
+    for _ in range(NUM_ITER):
+        contribs = rdd.join(ranks).flatMap(
+            lambda x: compute_contribs(x[1][0], x[1][1])
+        )
+
+        ranks = contribs.reduceByKey(lambda x, y: x + y)
+
+        ranks = ranks.mapValues(
+            lambda rank: (1 - BETA) / num_pages + BETA * rank
+        )
+
+    return ranks
 
 
 def main():
-    with open(sys.argv[1], "rt") as f:
-        lines = f.readlines()
+    conf = SparkConf()
+    sc = SparkContext(conf=conf)
 
-    adj = []
-    for line in lines:
-        edge = list(map(float, line.split()))
-        if edge not in adj:
-            adj.append(edge)
+    rdd = sc.textFile(sys.argv[1])
+    rdd = rdd.map(lambda x: x.split())
 
-    rdd = sc.parallelize(adj)
-    rdd = rdd.map(lambda x: (x[0], x[1]))
-    rdd = rdd.groupByKey()
-    rdd = rdd.mapValues(list)
+    rdd = rdd.filter(lambda x: len(x) == 2)
+    rdd = rdd.map(lambda x: (x[0], [x[1]]))
+    rdd = rdd.reduceByKey(lambda x, y: x + y)
+    rdd = rdd.map(lambda x: (x[0], sorted(list(set(x[1])))))
+
+    rdd = rdd.map(lambda x: (x[0], x[1], 1 / len(x[1])))
+
+    ranks = pagerank(rdd)
+
+    top10 = ranks.top(NUM_PRINT, key=lambda x: x[1])
+    for i in range(NUM_PRINT):
+        print(f"{top10[i][0]}\t{top10[i][1]:.5f}")
+
+    sc.stop()
 
 
 if __name__ == "__main__":
