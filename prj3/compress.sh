a7017e4f7b090cf68afb4acb34446859a92496f4
ddwkim <dwkim0824@gmail.com>
2023-11-18 02:07:42 +0900

    Add compress script

---
+++ prj3/compress.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define an array with the file names to be included in the tarball
+files=(
+    "hw3_1.py"
+    "hw3_2_p3.py"
+    "hw3_3_p3.py"
+    "hw3.pdf"
+    "Ethics Oath.pdf"
+)
+
+# Define the tarball name
+tarball_name="20170082_hw3.tar.gz"
+
+# Create the tarball with gzip compression
+tar -czvf "$tarball_name" "${files[@]}"
+
+echo "Tarball $tarball_name has been created successfully."
